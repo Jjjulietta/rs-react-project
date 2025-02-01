822ea78a5624c5765fd826c5849297bec5ef5730
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-01 10:20:31 +0300

    refactor: run prettier format command

--- rs-react-app/eslint.config.js
+++ rs-react-app/eslint.config.js
@@ -1,16 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import react from "eslint-plugin-react";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import reactCompiler from "eslint-plugin-react-compiler";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.strict, eslintPluginPrettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -18,10 +22,10 @@ export default tseslint.config(
     },
     plugins: {
       react,
-      "@typescript-eslint": tseslint.plugin,
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      "react-compiler": reactCompiler,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -29,14 +33,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      "react-compiler/react-compiler": "error",
+      'react-compiler/react-compiler': 'error',
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
+      ...react.configs['jsx-runtime'].rules,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
-  },
-)
+  }
+);
