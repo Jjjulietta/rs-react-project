36f2f277fd91a531017094ffeb5cc2d3e3a66d91
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-09 12:24:51 +0300

    feat: implement routing

--- rs-react-app/src/App.tsx
+++ rs-react-app/src/App.tsx
@@ -1,14 +1,24 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
-
-import Main from './components/Main/Main';
+import { Main } from './components/Main/Main';
+import { Details } from './components/Details/Details';
+import { NotFound } from './components/NotFound/NotFound';
 
 function App() {
   return (
     <>
       <div>
         <ErrorBoundary>
-          <Main props={null} />
+          <Routes>
+            <Route path="/" element={<Main />}>
+              <Route
+                path={`details/:uid/${location.search}`}
+                element={<Details />}
+              ></Route>
+            </Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
         </ErrorBoundary>
       </div>
     </>
