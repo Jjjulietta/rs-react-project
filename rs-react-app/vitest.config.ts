000db57c039ae2db13d03f5631085bff2ea3f9e4
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-09 17:12:24 +0300

    feat: add exclude files to vitest config

--- rs-react-app/vitest.config.ts
+++ rs-react-app/vitest.config.ts
@@ -1,9 +1,21 @@
-import { defineConfig } from 'vitest/config';
+import {
+  configDefaults,
+  coverageConfigDefaults,
+  defineConfig,
+} from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: 'tests/setup.ts',
+    exclude: [...configDefaults.exclude, 'src/App.tsx', 'src/main.tsx'],
+    coverage: {
+      exclude: [
+        'src/App.tsx',
+        'src/main.tsx',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 });
