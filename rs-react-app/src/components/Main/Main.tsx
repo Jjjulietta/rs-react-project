7100a0d8c5284c46f0532834dd5651a57420ca9b
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-09 12:20:23 +0300

    feat: rewrite class component and add functionality

--- rs-react-app/src/components/Main/Main.tsx
+++ rs-react-app/src/components/Main/Main.tsx
@@ -1,131 +1,141 @@
-import { Component } from 'react';
-import Search from '../Search/Search';
-import CardList from '../CardList/CardList';
+import { useEffect, useState } from 'react';
+import { Search } from '../Search/Search';
+import { CardList } from '../CardList/CardList';
 import HttpService from '../../services/http.service';
-import { SEARCH_VALUE, URLS } from '../../utils/constants';
-import { Seasons, Statuses } from '../../types/apiTypes';
+import {
+  FIRST_PAGE,
+  PAGE_SIZE,
+  PAGINATION_DEFAULT,
+  URLS,
+} from '../../utils/constants';
+import { PaginationI, Seasons, Statuses } from '../../types/apiTypes';
 import styles from './Main.module.css';
-import Button from '../Button/Button';
+import { Button } from '../Button/Button';
 import { AxiosError } from 'axios';
+import { Pagination } from '../Pagination/Pagination';
+import { Outlet, useNavigate, useParams, useSearchParams } from 'react-router';
 
-interface MainState {
-  value: string;
-  items: Seasons[] | [];
-  error: string | undefined;
-  errorStatus: string | undefined;
-  isLoading: boolean;
-}
+export const Main = () => {
+  const [value, setValue] = useState<string | null | undefined>(null);
+  const [items, setItems] = useState<Seasons[] | []>([]);
+  const [, setSearchParam] = useSearchParams();
+  const [paginationState, setPaginationState] =
+    useState<PaginationI>(PAGINATION_DEFAULT);
+  const [currentPage, setCurrentPage] = useState(PAGINATION_DEFAULT.pageNumber);
+  const [error, setError] = useState<string | undefined>();
+  const [errorStatus, setErrorStatus] = useState<string | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const params = useParams();
+  const navigate = useNavigate();
 
-interface Props {
-  props: null;
-}
+  useEffect(() => {
+    if (error) {
+      throw error;
+    }
+  }, [error]);
 
-export default class Main extends Component<Props, MainState> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      value: this.getLocalStorageData() || '',
-      items: [],
-      error: undefined,
-      errorStatus: undefined,
-      isLoading: false,
-    };
-  }
-
-  HttpService = new HttpService();
-  updateValue = (val: string) => {
-    this.setState({ value: val });
+  const httpService = new HttpService();
+  const updateValue = (val: string) => {
+    setValue(val);
+    getSearchValue(val, FIRST_PAGE, PAGE_SIZE);
   };
 
-  getData = () => {
-    this.setState({ isLoading: true });
-    this.HttpService.getPagination(URLS.PAGINATION)
+  const closeDetails = () => {
+    if (params.uid) {
+      navigate(`/${location.search}`);
+    }
+  };
+
+  const getData = (pageNumber?: number, pageSize?: number) => {
+    setIsLoading(true);
+    httpService
+      .getPagination(URLS.PAGINATION, { pageNumber, pageSize })
       .then((data) => {
-        this.setState({ items: data.seasons });
+        setItems(data.seasons);
+        setPaginationState(data.page);
       })
       .catch((e: AxiosError) => {
         if (
           e.status &&
           (e.status >= Statuses.ErrorClient || e.status >= Statuses.ErrorServer)
         ) {
-          this.setState({ errorStatus: e.message });
+          setErrorStatus(e.message);
         } else {
-          this.setState({ error: e.message });
+          setError(e.message);
         }
       })
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() => setIsLoading(false));
   };
 
-  getSearchValue = (value: string) => {
-    this.setState({ isLoading: true });
-    this.HttpService.SearchData(
-      URLS.SEARCH,
-      value,
-      { pageNumber: 0, pageSize: 10 },
-      {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }
-    )
+  const getSearchValue = (
+    value: string,
+    pageNumber: number,
+    pageSize: number
+  ) => {
+    setIsLoading(true);
+    httpService
+      .SearchData(
+        URLS.SEARCH,
+        value,
+        { pageNumber, pageSize },
+        {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      )
       .then((data) => {
-        this.setState({ items: data.data?.seasons });
+        console.log(data);
+        setItems(data.data?.seasons);
+        setPaginationState(data.data.page);
       })
       .catch((e: AxiosError) => {
         if (
           e.status &&
           (e.status >= Statuses.ErrorClient || e.status >= Statuses.ErrorServer)
         ) {
-          this.setState({ errorStatus: e.message });
+          setErrorStatus(e.message);
         } else {
-          this.setState({ error: e.message });
+          setError(e.message);
         }
       })
-      .finally(() => this.setState({ isLoading: false }));
+      .finally(() => setIsLoading(false));
   };
 
-  getLocalStorageData = () => localStorage.getItem(SEARCH_VALUE);
-
-  handleClick = () => {
-    this.setState({ error: 'Error!' });
+  const handleClick = () => {
+    setError('Error!');
   };
 
-  componentDidMount(): void {
-    if (this.state.value) {
-      this.getSearchValue(this.state.value);
+  useEffect(() => {
+    console.log(currentPage);
+    setSearchParam({ search: `${value}`, page: `${currentPage}` });
+    if (value) {
+      getSearchValue(value, currentPage - 1, PAGE_SIZE);
     } else {
-      this.getData();
+      getData(currentPage - 1, PAGE_SIZE);
     }
-  }
+    // eslint-disable-next-line react-compiler/react-compiler
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
 
-  componentDidUpdate(
-    _prevProps: Readonly<object>,
-    prevState: Readonly<MainState>
-  ): void {
-    console.log(prevState.value);
-    console.log(this.state.value);
-    if (prevState.value !== this.state.value) {
-      console.log(this.state.value);
-      this.getSearchValue(this.state.value || '');
-    }
-  }
-  render() {
-    if (this.state.error) {
-      throw this.state.error;
-    }
-    return (
-      <div>
-        <Search value={this.state.value || ''} onSearch={this.updateValue} />
-        <CardList
-          items={this.state.items}
-          error={this.state.errorStatus}
-          isLoading={this.state.isLoading}
-        />
+  return (
+    <div className={params.uid ? styles.layout : ''} onClick={closeDetails}>
+      <div className={params.uid ? styles.search_details : styles.search}>
+        <Search onSearch={updateValue} />
+        <CardList items={items} error={errorStatus} isLoading={isLoading} />
         <Button
           type="button"
           className={styles.btn_error}
-          onClick={this.handleClick}
+          onClick={handleClick}
           name="error button"
         />
+        {items.length && (
+          <Pagination
+            totalPage={paginationState.totalPages}
+            currentPage={currentPage}
+            setPage={setCurrentPage}
+          />
+        )}
       </div>
-    );
-  }
-}
+      <Outlet />
+    </div>
+  );
+};
