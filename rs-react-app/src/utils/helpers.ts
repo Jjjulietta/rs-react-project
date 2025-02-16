d346e4478c9eeb7e6ce78754d688abb8662aefc8
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-16 22:00:25 +0300

    feat: add helpers

--- rs-react-app/src/utils/helpers.ts
+++ rs-react-app/src/utils/helpers.ts
@@ -1,7 +1,71 @@
+import { EpisodeDetails, SeasonType, SeriesDetails } from '../types/apiTypes';
+
 export const stringTransform = (value: string) => {
   const arr = value
     .split(/(?!^)(?=[A-Z])/)
     .map((item) => `${item.toLowerCase()} `);
   const str = arr.join(' ').trim();
   return str;
+};
+
+export const convertToCSV = (objArray: (SeriesDetails | null)[]) => {
+  let csvString = '';
+  if (objArray.length && objArray.every((item) => item !== null)) {
+    const str = [
+      Object.keys(objArray[0]),
+      ...objArray.map((item) => Object.values(item)),
+    ]
+      .map((row) => row.join(','))
+      .join('\n');
+    csvString = str;
+  }
+  const csvData = new Blob([csvString], { type: 'text/csv' });
+  const csvURL = URL.createObjectURL(csvData);
+  return csvURL;
+};
+
+export const getDetails = (data: SeasonType) => {
+  const episodes = data.season.episodes.reduce<EpisodeDetails[]>(
+    (details, item) => {
+      details.push({
+        uid: item.uid,
+        title: item.title,
+        episodeNumber: item.episodeNumber,
+        productionSerialNumber: item.productionSerialNumber,
+        usAirDate: item.usAirDate,
+      });
+      return details;
+    },
+    []
+  );
+  const {
+    title,
+    abbreviation,
+    productionStartYear,
+    originalRunStartDate,
+    seasonsCount,
+    episodesCount,
+    featureLengthEpisodesCount,
+    productionCompany: { name: productionCompanyName },
+    originalBroadcaster: { name: originalBroadcasterName },
+  } = data.season.series;
+  const series: SeriesDetails = {
+    title,
+    abbreviation,
+    productionStartYear,
+    originalRunStartDate,
+    seasonsCount,
+    episodesCount,
+    featureLengthEpisodesCount,
+    productionCompanyName,
+    originalBroadcasterName,
+  };
+  return {
+    uid: data.season.uid,
+    title: data.season.title,
+    seasonNumber: data.season.seasonNumber,
+    series: series,
+    numberOfEpisodes: data.season.numberOfEpisodes,
+    episodes: episodes,
+  };
 };
