68e52370e570e5a5fd6c12d5090da3a0d18635b9
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-20 22:43:00 +0300

    feat: setup mcw

--- rs-react-app/tests/setup.ts
+++ rs-react-app/tests/setup.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom/vitest';
+import 'whatwg-fetch';
+import { setupStore } from '../src/app/store/store';
+import { server } from '../src/mocks/api/server';
+import { apiSlice } from '../src/app/store/apiSlice';
+import { cleanup } from '@testing-library/react';
+
+const store = setupStore({});
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+  store.dispatch(apiSlice.util.resetApiState());
+});
+
+afterAll(() => server.close());
