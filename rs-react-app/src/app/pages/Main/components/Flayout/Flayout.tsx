66b9dd812acf5bd7dd189f402f17d8be9c9049e7
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-02-21 20:21:12 +0300

    feat: create flayout component

---
+++ rs-react-app/src/app/pages/Main/components/Flayout/Flayout.tsx
@@ -0,0 +1,39 @@
+import {
+  removedAll,
+  selectCheckedNumber,
+} from '../../../../store/checkedSlice';
+import { selectAllDetails } from '../../../../store/detailsSlice';
+import { useAppDispatch, useAppSelector } from '../../../../store/withTypes';
+import { convertToCSV } from '../../../../shared/utils/helpers';
+import { Button } from '../../../../shared/components/Button/Button';
+import styles from './Flayout.module.css';
+
+export const Flayout = () => {
+  const checkedNumber = useAppSelector(selectCheckedNumber);
+  const detailsData = useAppSelector(selectAllDetails);
+  const series = detailsData.map((item) => item.series);
+  const dispatch = useAppDispatch();
+  const unselectAll = () => {
+    dispatch(removedAll([]));
+  };
+
+  return (
+    <div className={styles.container}>
+      <Button
+        className={styles.btn}
+        type="button"
+        name="Unselect all"
+        onClick={unselectAll}
+      />
+      <div data-testid="checkedNumber" className={styles.text}>
+        {checkedNumber} items are selected
+      </div>
+      <a
+        href={series ? convertToCSV(series) : '/'}
+        download={`${checkedNumber}_seasons.csv`}
+      >
+        <Button className={styles.btn} type="button" name="Download" />
+      </a>
+    </div>
+  );
+};
