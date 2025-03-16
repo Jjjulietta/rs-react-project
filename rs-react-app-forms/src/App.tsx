a44babb2c47c41caaf62c4b1ce8faacf834f2ca6
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-03-16 17:45:47 +0300

    feat: implement routing

--- rs-react-app-forms/src/App.tsx
+++ rs-react-app-forms/src/App.tsx
@@ -1,24 +1,18 @@
-import { useState } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { Main } from './app/pages/main/Main';
+import { Form } from './app/pages/form/Form';
+import { HookForm } from './app/pages/hook-form/Hook_form';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="form" element={<Form />} />
+        <Route path="hookform" element={<HookForm />} />
+      </Routes>
+    </div>
   );
 }
 
