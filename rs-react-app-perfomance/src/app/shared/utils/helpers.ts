6c587a2e6a25b4082573d6c84be4cc348ad96954
Yuliya Ivashchanka <jjjuliettai@gmail.com>
2025-03-19 19:40:34 +0300

    feat: create api service and helpers methods

---
+++ rs-react-app-perfomance/src/app/shared/utils/helpers.ts
@@ -0,0 +1,46 @@
+import { Order, SORT_FIELDS } from '../../models/constants';
+import { Country } from '../../models/types';
+
+export const getRegionsList = (countries: Country[]) => {
+  const set = new Set<string>();
+  countries.forEach((item) => set.add(item.region));
+  const list: string[] = Array.from(set);
+  list.unshift('all');
+  return list;
+};
+
+export const orderBy = (
+  order: string | null | undefined,
+  fields: string | null | undefined,
+  countries: Country[]
+) => {
+  const sortedCountries = [...countries];
+  if (fields === SORT_FIELDS.NAME) {
+    return order === Order.DESC
+      ? sortedCountries.sort((a, b) => b.name.localeCompare(a.name))
+      : sortedCountries.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (fields === SORT_FIELDS.POPULATION) {
+    return order === Order.DESC
+      ? sortedCountries.sort((a, b) => b.population - a.population)
+      : sortedCountries.sort((a, b) => a.population - b.population);
+  }
+
+  return sortedCountries;
+};
+
+export const filteredByRegions = (value: string, countries: Country[]) => {
+  if (value === 'all') {
+    return countries;
+  }
+  return countries.filter((country) => country.region === value);
+};
+
+export const searchedCountry = (search: string, countries: Country[]) => {
+  if (search) {
+    return countries.filter(({ name }) =>
+      name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+    );
+  }
+
+  return countries;
+};
